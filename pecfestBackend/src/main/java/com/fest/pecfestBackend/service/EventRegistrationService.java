f32815ae0cc29181ca29da42fcf0e84d70739328
himanshugaur17 <himanshugaur1215@gmail.com>
2020-09-14 09:20:03 +0530

    Changing team api

--- pecfestBackend/src/main/java/com/fest/pecfestBackend/service/EventRegistrationService.java
+++ pecfestBackend/src/main/java/com/fest/pecfestBackend/service/EventRegistrationService.java
@@ -48,6 +48,12 @@ public class EventRegistrationService {
                     return WrapperResponse.builder().httpStatus(HttpStatus.BAD_REQUEST).statusMessage("Team Name already exists").build();
                 }
                 else{
+                    List<Team> teams=teamRepo.findAllByEventId(eventId);
+                    for(Team team:teams){
+                        String duplicateRegistrations=team.getMemberPecFestIdList().parallelStream().filter(pecFestIds::contains).collect(Collectors.joining(", "));
+                        if(StringUtils.isNotEmpty(duplicateRegistrations))
+                            return WrapperResponse.builder().statusMessage(duplicateRegistrations+" have already been registered").build();
+                    }
                     teamRepo.save(Team.builder().eventId(eventId).leaderPecFestId(pecFestIds.get(0)).memberPecFestIdList(String.join(",", pecFestIds)).teamName(teamName)
                             .leaderId(userRepo.findByPecFestId(pecFestIds.get(0)).getId())
                             .build());
