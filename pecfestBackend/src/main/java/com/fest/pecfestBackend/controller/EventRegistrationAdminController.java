ee9b29f290ad6544e93202ed935117b5cdd026d2
himanshugaur17 <himanshugaur1215@gmail.com>
2020-09-16 13:34:34 +0530

    Modifying the event reg admin service

--- pecfestBackend/src/main/java/com/fest/pecfestBackend/controller/EventRegistrationAdminController.java
+++ pecfestBackend/src/main/java/com/fest/pecfestBackend/controller/EventRegistrationAdminController.java
@@ -1,5 +1,6 @@
 package com.fest.pecfestBackend.controller;
 
+import com.fest.pecfestBackend.enums.Club;
 import com.fest.pecfestBackend.response.WrapperResponse;
 import com.fest.pecfestBackend.service.EventRegistrationAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,9 @@ public class EventRegistrationAdminController {
     @Autowired
     private EventRegistrationAdminService eventRegAdminService;
 
-    @PostMapping("/{event_id}/{team_name}")
-    public WrapperResponse registerTeamForAnEvent(@PathVariable("event_id") Long eventId, @RequestBody List<String> pecFestIds, @RequestHeader("session_id") String sessionId,
-                                                  @PathVariable(value = "team_name") String teamName){
-        return eventRegAdminService.registerTeamForAnEvent(eventId,pecFestIds,teamName,sessionId);
+    @PostMapping("/{organizing_club}/{event_name}/{team_name}")
+    public WrapperResponse registerTeamForAnEvent(@PathVariable("event_name") String eventName, @RequestBody List<String> pecFestIds, @RequestHeader("session_id") String sessionId,
+                                                  @PathVariable(value = "team_name") String teamName,@PathVariable(value = "organizing_club") Club organizingClub){
+        return eventRegAdminService.registerTeamForAnEvent(eventName,pecFestIds,teamName,sessionId,organizingClub);
     }
 }
--- pecfestBackend/src/main/java/com/fest/pecfestBackend/service/EventRegistrationAdminService.java
+++ pecfestBackend/src/main/java/com/fest/pecfestBackend/service/EventRegistrationAdminService.java
@@ -5,6 +5,7 @@ import com.fest.pecfestBackend.entity.User;
 import com.fest.pecfestBackend.enums.Club;
 import com.fest.pecfestBackend.repository.EventRepo;
 import com.fest.pecfestBackend.response.WrapperResponse;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -22,15 +23,16 @@ public class EventRegistrationAdminService {
     private EventRepo eventRepo;
     @Autowired EventRegistrationService eventRegService;
 
-    public WrapperResponse registerTeamForAnEvent(Long eventId, List<String> pecFestIds, String teamName, String sessionId) {
+    public WrapperResponse registerTeamForAnEvent(String eventName, List<String> pecFestIds, String teamName, String sessionId,Club organizingClub) {
+
            User user=sessionService.verifySessionId(sessionId);
         if(!Optional.ofNullable(user).isPresent()|| Objects.isNull(user.getCoordinatingClubName())||user.getCoordinatingClubName().equals(Club.EMPTY))
             return WrapperResponse.builder().httpStatus(HttpStatus.FORBIDDEN).statusMessage("Not authorized").build();
-        Optional<Event> eventOptional=eventRepo.findById(eventId);
-        if(eventOptional.isPresent()){
-            Event event=eventOptional.get();
+        List<Event> eventList= eventRepo.findAllByEventNameAndOrganizingClub(eventName,organizingClub);
+        if(CollectionUtils.isNotEmpty(eventList)){
+            Event event=eventList.get(0);
             if(event.getOrganizingClub().equals(Club.ALL)||event.getOrganizingClub().equals(user.getCoordinatingClubName())){
-                    return eventRegService.registerTeamForAnEvent(eventId,pecFestIds,teamName,sessionId);
+                    return eventRegService.registerTeamForAnEvent(event.getEventID(),pecFestIds,teamName,sessionId);
             }
             else
                 return WrapperResponse.builder().httpStatus(HttpStatus.FORBIDDEN).statusMessage("Not authorized to modify "+event.getOrganizingClub()+" club's registrations").build();
