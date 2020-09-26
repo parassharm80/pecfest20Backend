62b6fb59e7f8ade99ff588871fd7a46f5a213eff
himanshugaur17 <himanshugaur1215@gmail.com>
2020-09-26 09:20:01 +0530

    Adding isAdmin in session Response

--- pecfestBackend/src/main/java/com/fest/pecfestBackend/controller/SessionController.java
+++ pecfestBackend/src/main/java/com/fest/pecfestBackend/controller/SessionController.java
@@ -1,5 +1,8 @@
 package com.fest.pecfestBackend.controller;
 
+import com.fest.pecfestBackend.entity.User;
+import com.fest.pecfestBackend.enums.Club;
+import com.fest.pecfestBackend.response.SessionResponse;
 import com.fest.pecfestBackend.service.SessionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +17,12 @@ public class SessionController {
     @Autowired
     private SessionService sessionService;
     @GetMapping("/verify")
-    public boolean verifySessionId(@RequestHeader(value = "session_id",required = false) String sessionId){
-        if(Objects.isNull(sessionId)||sessionId.length()<2)
-            return false;
-        return !Objects.isNull(sessionService.verifySessionId(sessionId));
+    public SessionResponse verifySessionId(@RequestHeader(value = "session_id",required = false) String sessionId){
+        User user=sessionService.verifySessionId(sessionId);
+        if(Objects.isNull(user))
+            return SessionResponse.builder().isAdmin(false).isLoggedIn(false).build();
+        if(Objects.isNull(user.getCoordinatingClubName())||user.getCoordinatingClubName().equals(Club.EMPTY))
+            return SessionResponse.builder().isAdmin(false).isLoggedIn(true).build();
+        return SessionResponse.builder().isAdmin(true).isLoggedIn(true).build();
     }
 }
